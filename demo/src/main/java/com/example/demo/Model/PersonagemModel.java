b435fa22a9f4bd213fc9b82f78919e2b48209ea5
Pedro Henrique Ribeiro Custódio <pedrohenrique11112004@hotmail.com>
2025-04-08 20:42:34 -0300

    SpringBoot configurado e inicialmente feito o model do personagem

--- demo/src/main/java/com/example/demo/Model/PersonagemModel.java
+++ demo/src/main/java/com/example/demo/Model/PersonagemModel.java
@@ -1,2 +1,66 @@
-package com.example.demo.Model;public class PersonagemModel {
+package com.example.demo.Model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class PersonagemModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+    private String nomeAventureiro;
+    private String level;
+    private String forca;
+    private String defesa;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeAventureiro() {
+        return nomeAventureiro;
+    }
+
+    public void setNomeAventureiro(String nomeAventureiro) {
+        this.nomeAventureiro = nomeAventureiro;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getForca() {
+        return forca;
+    }
+
+    public void setForca(String forca) {
+        this.forca = forca;
+    }
+
+    public String getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(String defesa) {
+        this.defesa = defesa;
+    }
 }
